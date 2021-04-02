81c045ee43ef13b180286acc8d5368b6b56a83f9
Joe Moceri <jmoceri34@gmail.com>
2021-04-01 18:49:03 -0700

    changes

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -2,8 +2,14 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Router } from '@angular/router';
-//import { createUnityInstance } from '../assets/demo/Build/WebGL.loader.js';
- 
+
+declare var unityCreateInstance: any; //from 'unityCreateInstance';
+
+// <script src="Build/demo.loader.js"></script>
+// <script src = "Build/demo.framework.js.gz"></script>
+
+//import * as unityJs from 'C:/Users/jmoce/source/repos/unity-effects-pack/src/assets/demo/Build/demo.loader.js';
+
 @Component({
     selector     : 'demo',
     templateUrl  : './demo.component.html',
@@ -20,8 +26,49 @@ export class DemoComponent implements OnInit
     }
 
   async ngOnInit() {
-    //const result = UnityLoader.instantiate("unityContainer", "assets/demo/Build/demo.json");
 
-    //console.log(result);
+    var buildUrl = "assets/demo/Build";
+    var config = {
+      dataUrl: buildUrl + "/demo.data.gz",
+      frameworkUrl: buildUrl + "/demo.framework.js.gz",
+      codeUrl: buildUrl + "/demo.wasm.gz",
+      streamingAssetsUrl: "StreamingAssets",
+      companyName: "JoeMoceri",
+      productName: "Unity Effects Pack",
+      productVersion: "0.1",
+      devicePixelRatio: 0
+    };
+
+    let container = document.querySelector("#unity-container") || new Element();
+    var canvas : HTMLElement = document.querySelector("#unity-canvas") || new HTMLElement();
+    var loadingBar : HTMLElement = document.querySelector("#unity-loading-bar") || new HTMLElement();
+    var progressBarFull : HTMLElement = document.querySelector("#unity-progress-bar-full") || new HTMLElement();
+    var fullscreenButton : HTMLElement = document.querySelector("#unity-fullscreen-button") || new HTMLElement();
+    var mobileWarning : HTMLElement = document.querySelector("#unity-mobile-warning") || new HTMLElement();
+
+    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+      container.className = "unity-mobile";
+      config.devicePixelRatio = 1;
+      mobileWarning.style.display = "block";
+      setTimeout(() => {
+        mobileWarning.style.display = "none";
+      }, 5000);
+    } else {
+      canvas.style.width = "960px";
+      canvas.style.height = "600px";
+    }
+    loadingBar.style.display = "block";
+
+    createUnityInstance(canvas, config, (progress: any) => {
+      progressBarFull.style.width = 100 * progress + "%";
+    }).then((unityInstance: any) => {
+      loadingBar.style.display = "none";
+      fullscreenButton.onclick = () => {
+        unityInstance.SetFullscreen(1);
+      };
+    }).catch((message: any) => {
+      alert(message);
+    });
+
   }
 }
